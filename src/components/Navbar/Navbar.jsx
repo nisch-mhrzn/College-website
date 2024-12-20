3093674122d4874a6f16ebe38b0bd822ef77e3a5
Nischal <nischal.maharjan1233@gmail.com>
2024-12-20 20:33:59 +0545

    `Added Navbar component and update app structure`

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import './Navbar.css'
+import logo from '../../assets/logo.png'
+
+
+const Navbar = () => {
+  return (
+    <nav className="container">
+        <img src={logo} alt='' className='logo'/>
+        <ul>
+            <li>Home</li>
+            <li>Programs</li>
+            <li>About us</li>
+            <li>Campus</li>
+            <li>Testimonials</li>
+            <li><button className='btn'>Contact Us</button></li>
+
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
